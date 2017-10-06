11764bcb15f0f5ea9477641ae182c1a149bba697
Arturo Cruz <cruzjart@gmail.com>
2017-10-06 17:27:45 -0500

    Generar Archivo .bat para poder correr el Task Schedule
    Para poder correr este script, debes de poner el path del proyecto y el path de de php.exe

---
+++ schedulle.bat
@@ -0,0 +1,9 @@
+:loop
+cd C:\xampp\htdocs\your_app
+C:\xampp\php\php.exe artisan schedule:run 1>> NUL 2>&1
+goto loop
+
+:loop
+cd C:\laragon\www\intranet
+C:\laragon\bin\php\php-5.6.30-Win32-VC11-x86\php.exe artisan schedule:run 1>> NUL 2>&1
+goto loop
